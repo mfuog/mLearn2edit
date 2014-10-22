a323bb37c2f0a183b223a4f3a32060d4a246d162
Myrtha Fuog <myrtha@fuog.net>
2014-10-22 18:24:50 +0200

    [Cleanup] Rename login buttons

--- logoutGroup.php
+++ logoutGroup.php
@@ -2,11 +2,11 @@
     <li class="list-group-item">
                 <span class="pull-left">
                     <?php if(isset($_SESSION['google_access_token'])) { ?>
-                        <a class="btn btn-google-plus" href="<?php echo $logoutURL ?>"><i class="fa fa-google"></i> | Logout from Google+</a>
+                        <a class="btn btn-google-plus" href="<?php echo $logoutURL ?>"><i class="fa fa-google"></i> | Logout</a>
                     <?php } elseif (isset($_SESSION['twitter_access_token'])) {?>
-                        <a class="btn btn-twitter" href="<?php echo $logoutURL ?>"><i class="fa fa-twitter"></i> | Logout from Twitter</a>
+                        <a class="btn btn-twitter" href="<?php echo $logoutURL ?>"><i class="fa fa-twitter"></i> | Logout</a>
                     <?php } elseif (isset($_SESSION['fb_session'])) {?>
-                        <a class="btn btn-facebook" href="<?php echo $logoutURL ?>"><i class="fa fa-facebook"></i> | Logout from Facebook</a>
+                        <a class="btn btn-facebook" href="<?php echo $logoutURL ?>"><i class="fa fa-facebook"></i> | Logout</a>
                     <?php } ?>
                 </span>
         <br><br>
