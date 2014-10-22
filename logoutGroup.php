c546166b9427fd5fd0df758cb2da8477132b9775
Myrtha Fuog <myrtha@fuog.net>
2014-10-22 12:31:42 +0200

    Include Twitter login & logout (admin)

---
+++ logoutGroup.php
@@ -0,0 +1,15 @@
+<ul class="list-group">
+    <li class="list-group-item">
+                <span class="pull-left">
+                    <?php if(isset($_SESSION['google_access_token'])) { ?>
+                        <a class="btn btn-google-plus" href="<?php echo $logoutURL ?>"><i class="fa fa-google"></i> | Logout from Google+</a>
+                    <?php } elseif ($_SESSION['twitter_access_token']) {?>
+                        <a class="btn btn-twitter" href="<?php echo $logoutURL ?>"><i class="fa fa-twitter"></i> | Logout from Twitter</a>
+                    <?php } elseif (isset($_SESSION['fb_session'])) {?>
+                        <a class="btn btn-facebook" href="<?php echo $logoutURL ?>"><i class="fa fa-facebook"></i> | Logout from Facebook</a>
+                    <?php } ?>
+                    To go back and choose another resource.
+                </span>
+        <br><br>
+    </li>
+</ul>
---
+++ twitter_callback.php
@@ -0,0 +1,42 @@
+<?php
+require_once __DIR__ . '/vendor/abraham/twitteroauth/twitteroauth/twitteroauth.php';
+require_once __DIR__ . '/config.php';
+
+if ( session_id() == '' ) {
+    $session = session_start();
+}
+# Set commonly used URLs
+$baseURL = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
+$homeURL = $baseURL . '/' . basename($_SERVER['SCRIPT_NAME']);
+$logoutURL = $baseURL . '/index.php?logout';
+
+# Twitter oauth: Exchange temporary for real access token
+if (isset($_GET['oauth_token'])) {
+
+    # use the user's previously stored temporary credentials here
+    $twitter = new TwitterOAuth(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET,
+        $_SESSION['twitter_request_token'], $_SESSION['twitter_request_token_secret']);
+    $version = $twitter->useAPIVersion("1.1");
+
+    # uses the oauth_token from the request
+    $credentials = $twitter->getAccessToken($_GET['oauth_verifier']);
+
+    # save real access token (user's credentials are normally to be stored in a database)
+    $_SESSION['twitter_access_token'] = $credentials['oauth_token'];
+    $_SESSION['twitter_access_token_secret'] = $credentials['oauth_token_secret'];
+}
+
+if (isset($_SESSION['twitter_access_token'])) {
+    $user = $twitter->get('account/verify_credentials');
+    printf('%s, you are logged in as: admin', $user->name);
+}?>
+
+<?php include('header.php')?>
+
+<div class ="centered">
+
+    <?php include('logoutGroup.php')?>
+
+</div><!--END centered-->
+
+<?php include('footer.php')?>
