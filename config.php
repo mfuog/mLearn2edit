e2f8f1b007bde53387f2f749f7a42fa41d175bcc
Myrtha Fuog <myrtha@fuog.net>
2014-10-26 20:00:44 +0100

    Set protocol dynamically

--- config.php
+++ config.php
@@ -37,7 +37,8 @@ define('FACEBOOK_APP_SECRET', 'fcf08b757efe2ef7f9d6c4d1b7d993b4');
 /**
  * Frequently used URLs.
  */
-define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']));
+$protocol = explode(':', $_SERVER['REQUEST_SCHEME'])[0];
+define('BASE_URL',  $protocol. '://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']));
 define('LOGOUT_URL', BASE_URL . '?logout');
 define('MLEARN4WEB', 'http://celtest1.lnu.se:3030');
 define('MLEARN4WEB_API_URL', MLEARN4WEB . '/mlearn4web');
